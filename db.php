f97999b83a214fd478a52cb4c6fe0d88baab0de9
Salamon Raja <lsaraja4@in.ibm.com>
2017-04-09 12:57:59 -0500

    change8

--- db.php
+++ db.php
@@ -3,7 +3,12 @@ $var1 = $_POST['field1'];
 $var2 = $_POST['field2'];
 
 
-echo "the Name of the User is" . $var1 . "<br/>";
-echo "the Email of the User is" . $var2 . "<br/>";
+ 
+$myfile = fopen("textdata.txt", "w") or die("Unable to open file!");
+
+fwrite($myfile, $var1);
+fwrite($myfile, $var2);
+fclose($myfile);
+ 
 
 ?>
