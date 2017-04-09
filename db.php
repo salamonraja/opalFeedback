062319276c14d25cf339970aec02e1cc4fdde4fd
Salamon Raja <lsaraja4@in.ibm.com>
2017-04-09 14:01:06 -0500

    hjgl342jig

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $var1 = $_POST['field1'];
 $var2 = $_POST['field2'];
 
 
-echo "the Name of the User is" . $var1 . "<br/>";
-echo "the Email of the User is" . $var2 . "<br/>";
+echo "the Name of the User is " . $var1 . "<br/>";
+echo "the Email of the User is " . $var2 . "<br/>";
 
 ?>
